3460fbd633b1b762d6d9751c8527b38e79b4a72b
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 02:07:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(num)
   range = (2..Integer.sqrt(num))
   range.each do |value|
+    if (value < 0)
+      value * -1
+    end
+    
     if (num % value == 0)
       return false
     end
